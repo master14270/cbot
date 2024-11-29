c58e63bac916fff6049a9369ba97082bf11aee7c
master14270 <jessie.smith14270@gmail.com>
2024-11-28 20:45:32 -0500

    chore: Removes unneeded checks for properties, TypeScript ensures our safety and wellbeing.

--- src/helpers.ts
+++ src/helpers.ts
@@ -23,14 +23,8 @@ export async function getAllValidCommands() {
 			const rawImport = await import(urlPath);
 			const command = rawImport.default as Command;
 
-			// Set a new item in the Collection with the key as the command name and the value as the exported module
-			if ("data" in command && "execute" in command) {
-				validCommands.push(command);
-			} else {
-				console.log(
-					`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-				);
-			}
+			// Store the result.
+			validCommands.push(command);
 		}
 	}
 
